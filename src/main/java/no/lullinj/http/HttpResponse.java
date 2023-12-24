a3431b27c624d941c82c90a86d250a29ecf1c0d2
Amund <Lullinj98@gmail.com>
2023-12-24 13:07:21 +0100

    Update `HttpResponse` and `HttpResponseBuilder` classes
    The `HttpResponse` class now has its fields initialized via the constructor.
    The `HttpResponseBuilder` class now supports dynamic addition of headers and also includes automatic computation and setting of the `content-length` header based on the body content.

--- src/main/java/no/lullinj/http/HttpResponse.java
+++ src/main/java/no/lullinj/http/HttpResponse.java
@@ -1,15 +1,22 @@
 package no.lullinj.http;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
 
 public class HttpResponse {
 
-    private final Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-    private  String body;
-    private  String statusLine;
+    private final Map<String, List<String>> headers;
+    private String body;
+    private String statusLine;
 
-    public HttpResponse() {
+    public HttpResponse(Map<String, List<String>> headers, String body, String statusLine) {
 
+        this.headers = headers;
+        this.body = body;
+        this.statusLine = statusLine;
+        Optional<String> optionalString = Optional.of("heik");
+        optionalString.orElse("awd" + "hei");
     }
 }
--- src/main/java/no/lullinj/http/HttpResponseBuilder.java
+++ src/main/java/no/lullinj/http/HttpResponseBuilder.java
@@ -1,15 +1,42 @@
 package no.lullinj.http;
 
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class HttpResponseBuilder {
 
-    private String body;
+    private byte[] body;
 
     private int statusCode;
 
     private String statusString;
 
-    private Map<String, List<String>> headers;
+    private Map<String, List<String>> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+
+    public void addHeader(String name, String value) {
+        if (!headers.containsKey(name)) {
+            headers.put(name, new ArrayList<>());
+        }
+        headers.get(name).add(value);
+
+    }
+
+    public void addHeader(String name, Collection<String> values) {
+        if (!headers.containsKey(name)) {
+            headers.put(name, new ArrayList<>());
+        }
+        headers.get(name).addAll(values);
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+        statusString = StatusCode.getStatusCodeMessage(statusCode);
+    }
+
+
+
+    public void setBody(String body) {
+        this.body = body.getBytes();
+        headers.put("content-length", List.of(String.valueOf(body.getBytes().length)));
+    }
 }
