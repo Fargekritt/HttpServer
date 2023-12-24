da727168872b30d5b4aa690becc70aeb3a05583b
Amund <Lullinj98@gmail.com>
2023-12-24 13:17:45 +0100

    Created test for basic status codes (200,300,400,500)

---
+++ src/test/java/no/lullinj/http/HttpResponseBuilderTest.java
@@ -0,0 +1,35 @@
+package no.lullinj.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpResponseBuilderTest {
+    @Test
+    void setStatusCode() {
+        HttpResponseBuilder responseBuilder = new HttpResponseBuilder();
+
+        responseBuilder.setStatusCode(200);
+        HttpResponse response = responseBuilder.buid();
+        assertEquals(response.getStatusCode(), 200);
+        assertEquals(response.getStatusMessage(), "OK");
+
+        responseBuilder.setStatusCode(300);
+        response = responseBuilder.buid();
+        assertEquals(response.getStatusCode(), 300);
+        assertEquals(response.getStatusMessage(), "MULTIPLE CHOICES");
+
+        responseBuilder.setStatusCode(400);
+        response = responseBuilder.buid();
+        assertEquals(response.getStatusCode(), 400);
+        assertEquals(response.getStatusMessage(), "BAD REQUEST");
+
+        responseBuilder.setStatusCode(500);
+        response = responseBuilder.buid();
+        assertEquals(response.getStatusCode(), 500);
+        assertEquals(response.getStatusMessage(), "INTERNAL SERVER ERROR");
+
+    }
+
+
+}
